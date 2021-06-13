8796432833b33622e40a64730b5490da20255a45
Morgan Lee <menghan.lee@ufl.edu>
2021-06-13 19:40:43 -0400

    Add process_image function

--- find_clearance.py
+++ find_clearance.py
@@ -2,6 +2,21 @@ import numpy as np
 import sys
 
 
+def process_image(img, lower=1, upper=4):
+    height, width = np.shape(img)
+    for i in range(height):
+        for j in range(width):
+            if img[i][j] > upper:
+                img[i][j] = 255
+            elif img[i][j] < lower:
+                img[i][j] = 0
+            else:
+                img[i][j] = 127
+
+    return img
+
+
 if __name__ == '__main__':
     file = sys.argv[1]
-    img = np.loadtxt(file)
+    depth_img = np.loadtxt(file)
+    img_processed = process_image(depth_img)
