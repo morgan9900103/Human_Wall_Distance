ff826396bbaacbbbee7cb82717953e83cd39a037
Morgan Lee <menghan.lee@ufl.edu>
2021-06-13 23:30:29 -0400

    Debugs

--- find_clearance.py
+++ find_clearance.py
@@ -1,3 +1,4 @@
+import cv2.detail
 import numpy as np
 import sys
 import cv2
@@ -9,7 +10,7 @@ def process_image(img, upper=4, lower=1):
         for j in range(width):
             if img[i][j] > upper:
                 img[i][j] = 255
-            elif img[i][j] < lower:
+            elif img[i][j] < 3:
                 img[i][j] = 0
             else:
                 img[i][j] = 127
@@ -21,25 +22,38 @@ def process_image(img, upper=4, lower=1):
 def find_clearance(img):
     img = np.uint8(img)
     avg = np.mean(img)
-    img_canny = cv2.Canny(img, avg - 0.25*avg, avg + 0.25*avg)
+    threshold1 = 0.75 * avg
+    threshold2 = 1.25 * avg
+    img_canny = cv2.Canny(img, threshold1, threshold2)
 
     height, width = np.shape(img)
     roi = np.zeros(shape=(height, width), dtype=np.uint8)
     roi[30:108, 60:120] = img_canny[30:108, 60:120]
 
     img_contour, contours, hierarchy = cv2.findContours(roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
 
     approx = [None]*len(contours)
     bound_rect = [None]*len(contours)
+    x = [0]*len(contours)
+    y = [0]*len(contours)
+    w = [0]*len(contours)
+    h = [0]*len(contours)
 
     for i, cnt in enumerate(contours):
         epsilon = 0.1 * cv2.arcLength(cnt, True)
         approx[i] = cv2.approxPolyDP(cnt, epsilon, True)
         bound_rect[i] = cv2.boundingRect(approx[i])
+        # x[i], y[i], w[i], h[i] = cv2.boundingRect(approx[i])
+        # cv2.rectangle(img_contour, (x[i], y[i]), (x[i]+w[i], y[i]+h[i]), (0, 255, 0), 5)
 
+    print(bound_rect)
     left = bound_rect[0][0] - 60
 
     right = 120 - (bound_rect[0][0] + bound_rect[0][2])
+
+    # cv2.imshow("img_contour", img_contour)
+    # cv2.waitKey(1)
 
     if left > right:
         print("left", left*1.5/60)
