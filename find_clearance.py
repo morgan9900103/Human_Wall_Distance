89b2ed8489bd7f66f85be6a7032dd1835b05ebe3
Morgan Lee <menghan.lee@ufl.edu>
2021-06-15 00:08:27 -0400

    Tune parameters

--- find_clearance.py
+++ find_clearance.py
@@ -1,64 +1,83 @@
-import cv2.detail
 import numpy as np
 import sys
 import cv2
 
 
-def process_image(img, upper=4, lower=1):
+def process_image(img, human_dist=2):
+    # Filter out the data points which are not in the same distance of human (around 2m)
     height, width = np.shape(img)
     for i in range(height):
         for j in range(width):
-            if img[i][j] > upper:
+            if img[i][j] > human_dist+2:
                 img[i][j] = 255
-            elif img[i][j] < 3:
+            elif img[i][j] < human_dist-1:
                 img[i][j] = 0
             else:
                 img[i][j] = 127
 
     img = np.uint8(img)
+    visual = True
+    if visual:
+        img_re = cv2.resize(img, (176*5, 132*5))
+        cv2.imshow("img", img_re)
+        cv2.waitKey()
     return img
 
 
-def find_clearance(img):
-    img = np.uint8(img)
+def find_clearance(img, human_dist=2, corridor=1.5):
+    # Canny edge detection
     avg = np.mean(img)
-    threshold1 = 0.75 * avg
-    threshold2 = 1.25 * avg
+    threshold1 = 0.8 * avg
+    threshold2 = 1.2 * avg
     img_canny = cv2.Canny(img, threshold1, threshold2)
+    visual = False
+    if visual:
+        img_canny_re = cv2.resize(img_canny, (176*5, 132*5))
+        cv2.imshow("img_canny", img_canny_re)
+        cv2.waitKey()
 
+    # Define size of roi
     height, width = np.shape(img)
     roi = np.zeros(shape=(height, width), dtype=np.uint8)
-    roi[30:108, 60:120] = img_canny[30:108, 60:120]
+    h1 = 0      # ceiling
+    h2 = 106    # ground
+    w1 = 60     # shelf
+    w2 = 122    # wall
+    roi[h1:h2, w1:w2] = img_canny[h1:h2, w1:w2]
+    visual = True
+    if visual:
+        roi_re = cv2.resize(roi, (176*5, 132*5))
+        cv2.imshow("roi", roi_re)
+        cv2.waitKey()
 
+    # Find Contour
     img_contour, contours, hierarchy = cv2.findContours(roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    visual = False
+    if visual:
+        img_contour_re = cv2.resize(img_contour, (176*5, 132*5))
+        cv2.imshow("img_contour", img_contour_re)
+        cv2.waitKey()
+
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
 
     approx = [None]*len(contours)
     bound_rect = [None]*len(contours)
-    x = [0]*len(contours)
-    y = [0]*len(contours)
-    w = [0]*len(contours)
-    h = [0]*len(contours)
+    print("len(contours", len(contours))
 
     for i, cnt in enumerate(contours):
-        epsilon = 0.1 * cv2.arcLength(cnt, True)
+        epsilon = 0.01 * cv2.arcLength(cnt, True)
         approx[i] = cv2.approxPolyDP(cnt, epsilon, True)
         bound_rect[i] = cv2.boundingRect(approx[i])
-        # x[i], y[i], w[i], h[i] = cv2.boundingRect(approx[i])
-        # cv2.rectangle(img_contour, (x[i], y[i]), (x[i]+w[i], y[i]+h[i]), (0, 255, 0), 5)
+        # print("bound_rect[i]", bound_rect[i])
 
-    print(bound_rect)
-    left = bound_rect[0][0] - 60
+    left = (bound_rect[0][0] - w1) * corridor / (w2 - w1)
 
-    right = 120 - (bound_rect[0][0] + bound_rect[0][2])
-
-    # cv2.imshow("img_contour", img_contour)
-    # cv2.waitKey(1)
+    right = (w2 - (bound_rect[0][0] + bound_rect[0][2])) * corridor / (w2 - w1)
 
     if left > right:
-        print("left", left*1.5/60)
+        print("left", left)
     else:
-        print("right", right*1.5/60)
+        print("right", right)
 
 
 if __name__ == '__main__':
